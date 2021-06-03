51969068e96176617351a0c8b71d979cee3c5d25
PhongHoangg <69662375+PhongHoangg@users.noreply.github.com>
2021-06-03 13:30:03 +0700

    Add files via upload

--- Predicting House Price.py
+++ Predicting House Price.py
@@ -5,7 +5,7 @@
 
 # ## Importing Libraries
 
-# In[59]:
+# In[55]:
 
 
 import numpy as np
@@ -16,14 +16,14 @@ import sklearn
 
 # ## Importing the Data Set and Observe the Data Structure
 
-# In[18]:
+# In[56]:
 
 
 housing = pd.read_csv('housing.csv')
 housing.head()
 
 
-# In[20]:
+# In[57]:
 
 
 housing.info()
@@ -33,7 +33,7 @@ housing.info()
 
 # ## Visualizing the data
 
-# In[31]:
+# In[58]:
 
 
 housing.hist(bins=50, figsize=(15,15))
@@ -44,14 +44,14 @@ plt.show()
 
 # ## Taking care of missing data
 
-# In[34]:
+# In[59]:
 
 
 median = housing["total_bedrooms"].median()
 housing["total_bedrooms"].fillna(median, inplace=True) 
 
 
-# In[35]:
+# In[60]:
 
 
 housing.info()
@@ -61,7 +61,7 @@ housing.info()
 
 # ## Seperating the independent values and dependent values
 
-# In[43]:
+# In[61]:
 
 
 X = housing.iloc[:, housing.columns != 'median_house_value' ].values
@@ -72,7 +72,7 @@ y = housing.iloc[:, -5].values
 
 # By simply use the `corr` method, we can compute the the standard correlation coefficient of each column with the `median_house_value`.
 
-# In[44]:
+# In[62]:
 
 
 corr = housing.corr()
@@ -82,7 +82,7 @@ corr["median_house_value"].sort_values(ascending=False)
 # From the correlation of these values, it seems that only the `median_income` affects directly to the housing price as it holding a correlation of 0.688, while every column else has a value near to 0, meaning that there is no
 # linear correlation with the `median_housing_income`. Therefore, we want to add some attribute to find the insights of the data.
 
-# In[45]:
+# In[63]:
 
 
 housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
@@ -91,7 +91,7 @@ housing["population_per_household"]=housing["population"]/housing["households"]
 housing.head()
 
 
-# In[46]:
+# In[64]:
 
 
 corr = housing.corr()
@@ -104,7 +104,7 @@ corr["median_house_value"].sort_values(ascending=False)
 
 # When we check the information of the data again, the ocean_proximity has the data type of non-number, creating problems when we try to train a model for our data set. So, using `OneHotEncoder`, we can transform these category values to the vectors with numeric value.
 
-# In[47]:
+# In[65]:
 
 
 from sklearn.compose import ColumnTransformer
@@ -113,7 +113,7 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [8])], remaind
 X = np.array(ct.fit_transform(X))
 
 
-# In[49]:
+# In[66]:
 
 
 print(X)
@@ -121,7 +121,7 @@ print(X)
 
 # ## Splitting the dataset into the Training set and Test set
 
-# In[52]:
+# In[67]:
 
 
 from sklearn.model_selection import train_test_split
@@ -130,25 +130,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 # ## Feature Scaling
 
-# In[62]:
+# In[68]:
 
 
 housing["total_rooms"].max()
 
 
-# In[63]:
+# In[69]:
 
 
 housing["total_rooms"].min()
 
 
-# In[64]:
+# In[70]:
 
 
 housing["median_income"].max()
 
 
-# In[65]:
+# In[71]:
 
 
 housing["median_income"].min()
@@ -156,7 +156,7 @@ housing["median_income"].min()
 
 # Our columns contains a different range of values, for example the columns `total_rooms`has a range from 2 to 39320, while the median income has a range between 0 and 15. Such different ranges can create problem for our Machine Learning algorithm to learn and train the data, so we need to apply a feature scaling called standardization into our data set.
 
-# In[66]:
+# In[72]:
 
 
 from sklearn.preprocessing import StandardScaler
@@ -167,38 +167,46 @@ X_test[:, 5:] = sc.transform(X_test[:, 5:])
 
 # # Select and Train a Model
 
-# In[76]:
+# In[73]:
 
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
+
+param_grid = [
+ {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+ {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+ ]
+forest_reg = RandomForestRegressor()
+
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error')
 grid_search.fit(X_train, y_train)
 
 
-# In[90]:
+# In[79]:
 
 
 print(grid_search.best_estimator_)
 
 
-# In[98]:
+# In[75]:
 
 
-forest_reg = RandomForestRegressor(max_features = 8, n_estimators = 30)
+forest_reg = RandomForestRegressor(max_features = 6, n_estimators = 30, random_state = 0)
 forest_reg.fit(X_train, y_train)
 
 
-# In[99]:
+# In[76]:
 
 
-np.set_printoptions(threshold= np.inf)
+np.set_printoptions(threshold= 2)
 y_pred = forest_reg.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 
 # After choosing the RandomForestRegressor with the help of GridSearch in determining the best parameters, we will measure the model's RMSE to see our result.
 
-# In[100]:
+# In[77]:
 
 
 from sklearn.metrics import mean_squared_error
@@ -207,4 +215,4 @@ lin_rmse = np.sqrt(lin_mse)
 lin_rmse
 
 
-# We get a very promising RMSE of 49248. However, further research need to include to determine the best estimators for this data set and get a better RMSE socre. 
+# We get a very promising RMSE of 49525. However, further research need to include to determine the best estimators for this data set and get a better RMSE socre. 
